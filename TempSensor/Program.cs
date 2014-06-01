e9222094ecb16b906b80c410f3550dc8d32c31af
jonathan_cuesto <34cuesto@gmail.com>
2014-05-31 22:57:20 -0400

    creación de objeto puerto serial y apertura del puerto

---
+++ TempSensor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace TempSensor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serial = new SerialPort("COM3");
+            serial.Open();
+        }
+    }
+}
